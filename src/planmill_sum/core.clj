eeec4ba5ebccdbd41a5650117003323ad10d7029
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2018-05-01 21:38:40 +0300

    convert date, add monetary styles

--- src/planmill_sum/core.clj
+++ src/planmill_sum/core.clj
@@ -3,6 +3,8 @@
   (require [clojure.data.xml :as xml]
            [dk.ative.docjure.spreadsheet :as s]))
 
+(def monetary-style-string "# ##0.00\" €\"")
+
 (defn parse-row [row]
   (map #(-> % :content first :content first) row))
 
@@ -10,13 +12,29 @@
   (let [is-string-num (re-find #"^[0-9]+\.?[0-9]*$" str)]
     (if (nil? is-string-num) str (read-string str))))
 
+(defn convert-date [row]
+  (let [datetime (first row)
+        date (re-find #"^[0-9-]+" datetime)]
+    (cons
+      (if (nil? date)
+        datetime
+        date)
+      (rest row))))
+
 (defn set-formulas! [sheet first-row]
   (loop [row first-row]
     (let [cell (s/select-cell (str "F" row) sheet)]
       (if (nil? cell)
-        nil
-        (do (.setCellFormula cell (str "B" row "*E" row))
+        row
+        (do
+          (.setCellFormula cell (str "B" row "*E" row))
           (recur (inc row)))))))
+
+(defn set-styles! [sheet style columns row-start row-end]
+  (doseq [row (range row-start row-end)
+          column columns]
+    (let [cell (s/select-cell (str column row) sheet)]
+      (s/set-cell-style! cell style))))
 
 (defn process-excel [source target]
   (let [xml-data (xml/parse-str (slurp source))
@@ -25,10 +43,14 @@
         numeric-data (->> parsed-data
                           (map rest)
                           (map #(map convert-number %))
+                          (map convert-date)
                           (map vec)
-                          (map #(conj % "")))
-        wb (s/create-workbook "Hours" numeric-data)]
-    (set-formulas! (s/select-sheet "Hours" wb) 2)
+                          (map #(conj % "Sum")))
+        wb (s/create-workbook "Hours" numeric-data)
+        monetary-style (s/create-cell-style! wb {:data-format monetary-style-string})
+        sheet (s/select-sheet "Hours" wb)
+        row-count (set-formulas! sheet 2)]
+    (set-styles! sheet monetary-style ["B" "F"] 2 row-count)
     (s/save-workbook! target wb)))
 
 (defn -main
