2cd2d9b8511d6b57f0d635dabea3459d191148ac
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2018-05-01 19:58:45 +0300

    read xml and produce excel with formulas

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [dk.ative/docjure "1.12.0"]]
+                 [dk.ative/docjure "1.12.0"]
+                 [org.clojure/data.xml "0.0.8"]]
   :main ^:skip-aot planmill-sum.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/planmill_sum/core.clj
+++ src/planmill_sum/core.clj
@@ -1,7 +1,36 @@
 (ns planmill-sum.core
-  (:gen-class))
+  (:gen-class)
+  (require [clojure.data.xml :as xml]
+           [dk.ative.docjure.spreadsheet :as s]))
+
+(defn parse-row [row]
+  (map #(-> % :content first :content first) row))
+
+(defn convert-number [str]
+  (let [is-string-num (re-find #"^[0-9]+\.?[0-9]*$" str)]
+    (if (nil? is-string-num) str (read-string str))))
+
+(defn set-formulas! [sheet first-row]
+  (loop [row first-row]
+    (let [cell (s/select-cell (str "F" row) sheet)]
+      (if (nil? cell)
+        nil
+        (do (.setCellFormula cell (str "B" row "*E" row))
+          (recur (inc row)))))))
+
+(defn process-excel [source target]
+  (let [xml-data (xml/parse-str (slurp source))
+        rows (-> xml-data :content (nth 2) :content first :content)
+        parsed-data (map #(-> % :content parse-row) rows)
+        numeric-data (->> parsed-data
+                          (map rest)
+                          (map #(map convert-number %))
+                          (map vec)
+                          (map #(conj % "")))
+        wb (s/create-workbook "Hours" numeric-data)]
+    (set-formulas! (s/select-sheet "Hours" wb) 2)
+    (s/save-workbook! target wb)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [source target]
+  (process-excel source target))
